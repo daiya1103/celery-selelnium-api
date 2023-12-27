4713ba2d2a579eb078cba16aa6a2a2deca95b2ff
daiya <doraemonyeah666@gmail.com>
2023-12-27 13:31:07 +0900

    80:8000

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,7 +3,5 @@
 echo "Apply database migrations"
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
-gunicorn config.wsgi:application --bind 127.0.0.1:8000
 
 exec "$@"
